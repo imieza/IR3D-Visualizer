bb018c6cbed2bbff2182abd65075111ecf384f91
fnahuelc <fnahuelc@gmail.com>
2016-09-06 00:54:17 -0300

    Add files via upload

---
+++ mainView.py
@@ -0,0 +1,79 @@
+from PySide import QtGui
+import sys
+
+from plotWidget import PlotWidget
+from dataProcessing import DataProcessing
+
+class MainView(QtGui.QWidget):
+    def __init__(self, function=None):
+        super(MainView, self).__init__()
+        self.setWindowTitle('Impulse response 3D')
+        self.setObjectName('MainWidget')
+        self.setStyleSheet('QWidget#MainWidget {background-color: rgb(191, 191, 191)}')
+
+        self.function = function
+        self.widMatplot = PlotWidget(self.function)
+        self.dataProcceser = DataProcessing()
+
+        self._loadSignalsButton = QtGui.QPushButton('Open WAV B-Format File ')
+        self.filterFrequencyResponsePlotButton = QtGui.QPushButton('Low-Pass Filtering Response')
+        self.spectrogramPlotButton = QtGui.QPushButton('Spectogram')
+        self.filterFrequencyResponseWithStarsPlotButton = QtGui.QPushButton('Window Selection')
+        self.impulseResponse3DButton = QtGui.QPushButton('View IR3D Graph')
+
+        self._setLayout()
+
+        self.impulseResponse3DButton.clicked.connect(self._plotIR3D)
+        self._loadSignalsButton.clicked.connect(self._showLoadSignalsDialog)
+        self.spectrogramPlotButton.clicked.connect(self.showSpectogram)
+        self.filterFrequencyResponsePlotButton.clicked.connect(self.showLowPassFilteringResponse)
+        self.filterFrequencyResponseWithStarsPlotButton.clicked.connect(self.showSignalWithStarts)
+        self.show()
+
+        self.audio = None
+
+    def _showLoadSignalsDialog(self):
+        fileName = QtGui.QFileDialog.getOpenFileNames(self, ("Open File"), "/home/", ("Wav Files (*.wav)"))
+
+        if fileName[0] != []:
+            [self.audio, self.fs] = self.dataProcceser.load_wavefile(fileName[0][0])
+            self.audio_filtered = self.dataProcceser.low_filtering(self.audio, self.fs)
+            self.widMatplot.plot(self.audio, self.fs, "Audio")
+
+    def _plotIR3D(self):
+        self.widMatplot.ploter3d(self.normalizado, self.az_el_windows[:, self.index_of_peaks])
+
+    def showSpectogram(self):
+        self.widMatplot.plot_spectrogram(self.audio, self.fs)
+
+    def showLowPassFilteringResponse(self):
+        self.widMatplot.plotLowFilteredSignals(self.dataProcceser.filterFrequency, self.dataProcceser.filterAmplitudeResponse)
+
+    def showSignalWithStarts(self):
+        # audio_filtered = self.widMatplot.low_filtering(self.audio, False, self.fs)
+        intensity = self.dataProcceser.pressure_to_intensity(self.audio_filtered)
+        [self.intensity_windows, self.az_el_windows, i_db] = self.dataProcceser.temporal_windowing(intensity, self.fs)
+        self.index_of_peaks = self.dataProcceser.window_selector(i_db, self.fs)
+        self.normalizado = self.dataProcceser.min_vector_be_equal_to_0(i_db[0, self.index_of_peaks])
+
+        self.widMatplot.plot(self.audio, self.fs, "Audio with windows",self.index_of_peaks)
+
+    def _setLayout(self):
+        hLayout = QtGui.QHBoxLayout()
+
+        hLayout.addWidget(self._loadSignalsButton)
+        hLayout.addWidget(self.filterFrequencyResponsePlotButton)
+        hLayout.addWidget(self.spectrogramPlotButton)
+        hLayout.addWidget(self.filterFrequencyResponseWithStarsPlotButton)
+        hLayout.addWidget(self.impulseResponse3DButton)
+
+        self.setLayout(QtGui.QGridLayout())
+        self.layout().addLayout(hLayout, 0, 0, 1, 1)
+        self.layout().addWidget(self.widMatplot, 1, 0, 1, 1)
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    main = MainView()
+    main.show()
+    sys.exit(app.exec_())
---
+++ plotWidget.py
@@ -0,0 +1,107 @@
+import matplotlib
+import numpy as numpy
+from PySide import QtGui
+
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4'] = 'PySide'
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+
+
+class PlotWidget(QtGui.QWidget):
+    time_window = 1  # Longitud de cada ventana temporal en ms
+    number_of_windows = 256
+    cutoff_frequency = 5000
+    trucate_value = None
+
+    def __init__(self, parent=None):
+        plt.clf()
+
+        super(PlotWidget, self).__init__(parent)
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+    def plot(self, data, fs, title, peaks=None):
+        plt.clf()
+        time = numpy.arange(0.0, float(len(data.tolist()[0])) / fs, 1.0 / fs)
+        for channel in range(len(data)):
+            ax = self.figure.add_subplot(len(data) * 100 + 11 + channel)
+            if peaks is not None:
+                ax.plot(time, data.tolist()[channel], "b-", time[peaks], data[:, peaks].tolist()[channel], "r*")
+            else:
+                ax.plot(time, data.tolist()[channel])
+
+        plt.subplots_adjust(hspace=1)
+        self.canvas.draw()
+
+    def ploter3d(self, i_db, az_el_windows):
+        from mpl_toolkits.mplot3d import Axes3D
+
+        plt.clf()
+        fig = plt.figure(dpi=100)
+        ax = self.figure.add_subplot(111, projection='3d')
+        [x, y, z] = self.sph2cart(az_el_windows[0], az_el_windows[1], i_db)
+        colors_of_red = 20
+        color = iter(plt.cm.Spectral(numpy.linspace(0.2, 1, len(x) + colors_of_red)))
+        for i in range(1, len(x)):
+            c = next(color)
+            ax.plot([0, x[i]], [0, y[i]], [0, z[i]], marker="_", c=c, linewidth=1.5)
+        ax.plot([0, x[0]], [0, y[0]], [0, z[0]], marker="_", c='r', linewidth=5.0)
+        self.axisEqual3D(ax)
+        self.canvas.draw()
+
+    def axisEqual3D(self, ax):
+        plt.clf()
+        extents = numpy.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
+        sz = extents[:, 1] - extents[:, 0]
+        centers = numpy.mean(extents, axis=1)
+        maxsize = max(abs(sz))
+        r = maxsize / 2
+        for ctr, dim in zip(centers, 'xyz'):
+            getattr(ax, 'set_{}lim'.format(dim))(ctr - r, ctr + r)
+
+    def plot_spectrogram(self, data, fs):
+        plt.clf()
+        title = ["W Spectrogram", "X Spectrogram", "Y Spectrogram", "Z Spectrogram"]
+        NFFT = 2048
+
+        for channel in range(len(data)):
+            x = data[channel]
+            x = numpy.array(x.tolist()[0])
+
+            for index in range(len(x)):
+                if abs(x[index]) < 0.00001:
+                    x[index] = 0.00001
+                else:
+                    x[index] = (x[index])
+
+            plt.subplot(411 + channel)
+            plt.title(title[channel])
+            plt.subplots_adjust(hspace=1)
+            Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=fs, noverlap=10)
+
+        self.canvas.draw()
+
+    def plotLowFilteredSignals(self, filterFrequency, filterAmplitudeResponse):
+        plt.clf()
+
+        plt.subplot(111)
+        plt.semilogx(filterFrequency, 20 * numpy.log10(abs(filterAmplitudeResponse)))
+        plt.title('Butterworth filter frequency response')
+        plt.xlabel('Frequency [radians / second]')
+        plt.ylabel('Amplitude [dB]')
+        plt.margins(0, 0.1)
+        plt.grid(which='both', axis='both')
+        plt.axvline(100, color='green')  # cutoff frequency
+
+        self.canvas.draw()
+
+    def sph2cart(self, azimuth, elevation, r):
+        x = r * numpy.cos(elevation) * numpy.cos(azimuth)
+        y = r * numpy.cos(elevation) * numpy.sin(azimuth)
+        z = r * numpy.sin(elevation)
+        return x, y, z
+
